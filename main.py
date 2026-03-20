222a8f59e42860519295e25b81cc0df7d8ccb20b
Remxver1337 <egornovikov067@gmail.com>
2026-03-20 15:13:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -725,7 +725,7 @@ class SpamBot:
 
 # Запуск бота
 if __name__ == "__main__":
-    BOT_TOKEN = "8337608591:AAG6SeAOY5PIVkcy_xljTR-J2KyKEtzLgSI"
+    BOT_TOKEN = "8695515419:AAEUM1_b6SE0T4ytTSu5lX0dDEwUKdqO_0Q"
     
     bot = SpamBot(BOT_TOKEN)
     print("🤖 Бот запущен и готов к работе!")
